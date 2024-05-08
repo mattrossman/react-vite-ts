10b69ab70b445a16c01d180c77208ea7750cbab6
Matt Rossman <22670878+mattrossman@users.noreply.github.com>
2024-05-08 08:30:39 -0400

    Add prettier and tailwind plugin

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 export function App() {
   return (
-    <div className="bg-gradient-to-br from-green-600 to-blue-400 inset-0 fixed grid place-items-center">
-      <p className="text-white font-bold text-3xl">Vite + React + TS</p>
+    <div className="fixed inset-0 grid place-items-center bg-gradient-to-br from-green-600 to-blue-400">
+      <p className="text-3xl font-bold text-white">Vite + React + TS</p>
     </div>
   )
 }
